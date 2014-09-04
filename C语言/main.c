b956f7d8c1481c1870a84a1ce0f91c32167c7d48
Liu Xiwang <1574721517@qq.com>
2014-09-04 09:07:27 +0800

    Initial Commit

---
+++ C语言/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C语言
+//
+//  Created by user on 14-9-4.
+//  Copyright (c) 2014年 user. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
